fffc32fcfe38558309f81f85ec35dfffd84c655d
Wendel <wendelljuliao@hotmail.com>
2021-09-14 18:24:36 -0300

    cores

--- cenario2/src/Componentes/Header.js
+++ cenario2/src/Componentes/Header.js
@@ -9,15 +9,17 @@ export default function Header() {
     return (
         <Navbar bg="black" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">
-                    <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" height="50px" />
+                <Navbar.Brand>
+                    <Link to="/">
+                        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" height="50px" />
+                    </Link>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{backgroundColor: "rgb(26 178 107);"}}/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ backgroundColor: "rgb(26 178 107);" }} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link></Link>
-                        <Nav.Link href="App.js" style={{color: "white", fontSize: "20px"}}>Home</Nav.Link>
-                        <Nav.Link href='#' style={{color: "white", fontSize: "20px"}}>FAQ</Nav.Link>
+
+                        <Nav.Link href="App.js" style={{ color: "white", fontSize: "20px"}}><Link to="/" style={{textDecoration: "none", color: "white"}}>Home</Link></Nav.Link>
+                        <Nav.Link href="../faq.js" style={{ color: "white", fontSize: "20px" }}><Link to="/faq" style={{textDecoration: "none", color: "white"}}>Faq</Link></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
