df8a0b1675ba78df9ec1a008fe3579d2a70e1c01
Wendel <wendelljuliao@hotmail.com>
2021-09-16 20:53:33 -0300

    componente pergunta

---
+++ cenario2/src/Componentes/Pergunta.js
@@ -0,0 +1,41 @@
+import './Pages/Faq.css'
+import React, { Component } from "react";
+
+
+export default class Pergunta extends Component {
+
+    constructor(props) {
+        super(props);
+        this.click = this.click.bind(this)
+
+        this.state = {
+            
+        }
+
+
+    }
+
+    click() {
+        var elemento = document.getElementById("ites-faq"+this.props.id)
+        if (elemento.style.display == "flex")
+            elemento.style = "display: none;";
+        else
+            elemento.style = "display: flex;"
+    }
+
+    render() {
+        return (
+            <li onClick={this.click}>
+                <a class="assunto-faq" href="javascript://">{this.props.title}<svg xmlns="http://www.w3.org/2000/svg" width="16"
+                    height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                </svg></a>
+                <ul class="itens-faq1" id={"ites-faq"+this.props.id}>
+                    <label>{this.props.descricao}</label>
+                </ul>
+            </li>
+        )
+    }
+
+}
