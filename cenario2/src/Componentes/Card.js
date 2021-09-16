21c8da208462343338287a42b36661767b09c023
Wendel <wendelljuliao@hotmail.com>
2021-09-16 18:35:04 -0300

    gambiarra

--- cenario2/src/Componentes/Card.js
+++ cenario2/src/Componentes/Card.js
@@ -8,34 +8,54 @@ export default class Card extends Component {
 
     constructor(props) {
         super(props);
+        this.teste = this.teste.bind(this)
 
         this.state = {
             title_valor: "",
             sub_valor: "",
             bgimg_valor: "",
+            num: props.num,
         }
+
+
 
     }
 
-    
+    teste() {
+        if (this.props.num == "1") {
+            document.getElementById("popup1").style.display = 'flex'
+            document.getElementById("popup2").style.display = 'none'
+            document.getElementById("popup3").style.display = 'none'
+        } else if (this.props.num == "2") {
+            document.getElementById("popup1").style.display = 'none'
+            document.getElementById("popup2").style.display = 'flex'
+            document.getElementById("popup3").style.display = 'none'
+        } else if (this.props.num == "3") {
+            document.getElementById("popup1").style.display = 'none'
+            document.getElementById("popup2").style.display = 'none'
+            document.getElementById("popup3").style.display = 'flex'
+        }
+    }
+
+
     render() {
         return (
-                <div class="escopo-primary">
-                    <div class="escopo-secundary">
-                        <div class="line" onClick={function() {document.getElementById("popup").style.display = 'flex'}}>
-                            <div class="body" style={{backgroundImage: `url(${this.props.bgimg})`}}>
-                                <video autoPlay muted>
-                                    <source src={video} type="video/mp4"/>
-                                </video>
-                                <label class="title-card">{this.props.title}</label>
-                                <label class="sub-card">{this.props.sub}</label>
-                            </div>
-                            <label class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non nisl quis magna vehicula blandit. Sed vitae mattis diam. Aenean dapibus nibh tellus, vitae pretium metus sollicitudin et.</label>
+            <div class="escopo-primary">
+                <div class="escopo-secundary">
+                    <div class="line" onClick={this.teste}>
+                        <div class="body" style={{ backgroundImage: `url(${this.props.bgimg})` }}>
+                            <video autoPlay muted>
+                                <source src={video} type="video/mp4" />
+                            </video>
+                            <label class="title-card">{this.props.title}</label>
+                            <label class="sub-card">{this.props.sub}</label>
                         </div>
+                        <label class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non nisl quis magna vehicula blandit. Sed vitae mattis diam. Aenean dapibus nibh tellus, vitae pretium metus sollicitudin et.</label>
                     </div>
-                    <Popup title={"Lorem Ipsum" + this.props.num} sub="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non nisl quis magna vehicula blandit. Sed vitae mattis diam. Aenean dapibus nibh tellus, vitae pretium metus sollicitudin et." />
-
                 </div>
+                <Popup id="1" title={"Lorem Ipsum"} sub="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non nisl quis magna vehicula blandit. Sed vitae mattis diam. Aenean dapibus nibh tellus, vitae pretium metus sollicitudin et." />
+
+            </div>
         )
     }
 }
--- cenario2/src/Componentes/Popup.js
+++ cenario2/src/Componentes/Popup.js
@@ -2,9 +2,9 @@ import './Popup.css'
 import video from '../Images/video.mp4'
 import { Component } from 'react';
 
-export default class Popup extends Component{
+export default class Popup extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -15,23 +15,50 @@ export default class Popup extends Component{
         }
     }
 
-    
+
     render() {
         return (
-            
-            <div class="popup-bg" id={"popup"} >
+            <div>
+                <div class="popup-bg" id={"popup1"} >
+                    <div class="escopo-popup">
+                        <video muted autoPlay loop>
+                            <source src={video} type="video/mp4" />
+                        </video>
+                        <a href="#" id="close" onClick={function () { document.getElementById("popup1").style.display = 'none' }}></a>
+                        <div style={{ width: '100%' }}>
+                            <label class="title-popup">{this.props.title+"1"}</label>
+                            <label class="sub-popup">{this.props.sub}</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="popup-bg" id={"popup2"} >
                 <div class="escopo-popup">
                     <video muted autoPlay loop>
                         <source src={video} type="video/mp4"/>
                     </video>
-                    <a href="#" id="close" onClick={function() {document.getElementById("popup").style.display = 'none'}}></a>
+                    <a href="#" id="close" onClick={function() {document.getElementById("popup2").style.display = 'none'}}></a>
                     <div style={{width: '100%'}}>
-                        <label class="title-popup">{this.props.title}</label>
+                        <label class="title-popup">{this.props.title+"2"}</label>
                         <label class="sub-popup">{this.props.sub}</label>
                     </div>
                 </div>
             </div>
-        
+
+            <div class="popup-bg" id={"popup3"} >
+                <div class="escopo-popup">
+                    <video muted autoPlay loop>
+                        <source src={video} type="video/mp4"/>
+                    </video>
+                    <a href="#" id="close" onClick={function() {document.getElementById("popup3").style.display = 'none'}}></a>
+                    <div style={{width: '100%'}}>
+                        <label class="title-popup">{this.props.title+"3"}</label>
+                        <label class="sub-popup">{this.props.sub}</label>
+                    </div>
+                </div>
+            </div>
+            </div>
+
         )
     }
 }
