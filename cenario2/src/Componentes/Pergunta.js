548872a96db2c283ac4ceb942cf4ee1cde8047bc
GabrielCunha6403 <gabrielcunha2012@gmail.com>
2021-09-17 11:11:02 -0300

    componentizando perguntas faq

--- cenario2/src/Componentes/Pergunta.js
+++ cenario2/src/Componentes/Pergunta.js
@@ -1,4 +1,4 @@
-import './Pages/Faq.css'
+import './Pergunta.css'
 import React, { Component } from "react";
 
 
@@ -9,32 +9,40 @@ export default class Pergunta extends Component {
         this.click = this.click.bind(this)
 
         this.state = {
-            
+            title_valor: "",
+            sub_valor: "",
         }
 
 
     }
 
     click() {
-        var elemento = document.getElementById("ites-faq"+this.props.id)
-        if (elemento.style.display == "flex")
-            elemento.style = "display: none;";
-        else
-            elemento.style = "display: flex;"
+        var arrow = document.getElementById("arrow"+this.props.id)
+        var elemento = document.getElementById("itens-faq"+this.props.id)
+        if (elemento.style.height == ""){
+            arrow.style = "transform: rotate(180deg);"
+            elemento.style = "padding: 10px 30px !important; height: auto;";
+        }else{
+            arrow.style = "transform: rotate(0deg);"
+            elemento.style = "height: 0;"
+            elemento.style = "padding: 0;";
+        }
     }
 
     render() {
         return (
-            <li onClick={this.click}>
-                <a class="assunto-faq" href="javascript://">{this.props.title}<svg xmlns="http://www.w3.org/2000/svg" width="16"
+            <div class="faq">    
+                <li onClick= {this.click}>
+                    <a class="assunto-faq1" href="javascript://">{this.props.title}<svg id={"arrow"+this.props.id} xmlns="http://www.w3.org/2000/svg" width="16"
                     height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
-                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
-                </svg></a>
-                <ul class="itens-faq1" id={"ites-faq"+this.props.id}>
-                    <label>{this.props.descricao}</label>
-                </ul>
-            </li>
+                    d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                    </svg></a>
+                    <ul class="itens-faq" id={"itens-faq"+this.props.id}>
+                        <label>{this.props.sub}</label>
+                    </ul>
+                </li>
+            </div>
         )
     }
 
