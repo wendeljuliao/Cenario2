71e73c3d7a2bd0f710165149dfd32e3c70b8edd3
GabrielCunha6403 <gabrielcunha2012@gmail.com>
2021-10-01 19:36:10 -0300

    forms refactor

--- cenario2/src/Componentes/Pages/js/Formulario.js
+++ cenario2/src/Componentes/Pages/js/Formulario.js
@@ -1,9 +1,9 @@
 import Birth_day from '../../Componentes_Forms/Birth_day';
-import Label_Input from '../../Componentes_Forms/Label_Input';
 import Radio_Box from '../../Componentes_Forms/Radio_Box';
 import Title from '../../Componentes_Forms/Title';
 import CheckBox from '../../Componentes_Forms/CheckBox';
 import '../../../App.css'
+import '../css/Formulario.css'
 
 
 function Formulario() {
@@ -13,13 +13,73 @@ function Formulario() {
             <Title />
             <div className='ins' className="escopo-secundary">
                 <form action="/action_page.php">
-                <Label_Input title='Qual é o seu e-mail?' type='email' />
-                <Label_Input title='Confirme o seu e-mail' type='email' />
-                <Label_Input title='Crie uma senha' type='password' />
-                <Label_Input title='Como devemos chamar você?' type='text' />
-                <Birth_day />
-                <Radio_Box />
-                <CheckBox />
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>E-mail</label>
+                    <input type="email" class="bg-transparent border-0" style={{width: "100%"}}/>
+                  </div>
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>Confirme seu E-mail</label>
+                    <input type="email" class="bg-transparent border-0" style={{width: "100%"}}/>
+                  </div>
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>Crie uma senha</label>
+                    <input type="password" class="bg-transparent border-0" style={{width: "100%"}}/>
+                  </div>
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>Como devemos chamar você</label>
+                    <input class="bg-transparent border-0" style={{width: "100%"}}/>
+                  </div>
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>Data de nascimento</label>
+                    <div className="d-flex justify-content-center flex-row">
+                      <input class="bg-transparent border-0" type="text" style={{width: "100%"}}/>
+                      <select class="bg-transparent border-0 mx-3" style={{width: "100%"}}>
+                        <option className='nascimento-input-option'>Mês</option>
+                        <option className='nascimento-input-option'>Janeiro</option>
+                        <option className='nascimento-input-option'>Fevereiro</option>
+                        <option className='nascimento-input-option'>Março</option>
+                        <option className='nascimento-input-option'>Abril</option>
+                        <option className='nascimento-input-option'>Maio</option>
+                        <option className='nascimento-input-option'>Junho</option>
+                        <option className='nascimento-input-option'>Julho</option>
+                        <option className='nascimento-input-option'>Agosto</option>
+                        <option className='nascimento-input-option'>Setembro</option>
+                        <option className='nascimento-input-option'>Outubro</option>
+                        <option className='nascimento-input-option'>Novembro</option>
+                        <option className='nascimento-input-option'>Dezembro</option>
+                      </select>
+                      <input class="bg-transparent border-0" type="text" style={{width: "100%"}}/>
+                    </div>
+                  </div>
+                  <div className="inputField d-flex align-items-start flex-column">
+                    <label>Gênero</label>
+                    <div className="d-flex justify-content-between flex-row align-items-center" style={{width: "100%"}}>
+                      <div className="d-flex align-items-center">  
+                        <input type="radio" name="sexo"/>
+                        <label className="m-1">Masculino</label>
+                      </div>
+                      <div className="d-flex align-items-center">  
+                        <input type="radio" name="sexo"/>
+                        <label className="m-1">Feminino</label>
+                      </div>
+                      <div className="d-flex align-items-center">  
+                        <input type="radio" name="sexo"/>
+                        <label className="m-1">Não Binário</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='inside_elements1 justify-content-start py-5'>
+                  <div>
+                    <div className="align-items-center">
+                      <input type="checkbox" />
+                      <label className="mr-1" for="marketing"> Quero Compartilhar dados cadastrais o suporte do Spotify.</label>
+                    </div>
+                    <div className="align-items-center">
+                      <input className="mr-1" type="checkbox" />
+                      <label for="marketing"> Eu concordo com os <a href="" style={{color: "#26C77B" }}>Termos e Condições de Uso do Spotify.</a></label>
+                    </div>
+                  </div>
+                </div>
 
                 </form>
                 <input type="submit" style={{margin:12}} value="Inscreva-se" class="inscrevase" />
