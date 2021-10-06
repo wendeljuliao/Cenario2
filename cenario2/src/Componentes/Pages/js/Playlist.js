848eee6ee1de8bd658dce46f1b80286394897de5
GabrielCunha6403 <gabrielcunha2012@gmail.com>
2021-10-06 14:23:07 -0300

    formatação do player

--- cenario2/src/Componentes/Pages/js/Playlist.js
+++ cenario2/src/Componentes/Pages/js/Playlist.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useParams } from "react-router";
+import '../css/Playlist.css'
 
 import playlists from "../../../Mocks/playlist";
+
 
 export default function Playlist() {
     const { id } = useParams();
@@ -9,10 +11,16 @@ export default function Playlist() {
     const playlist = playlists.find((p) => p.id == id)
     return (
         <div class="escopo-primary">
-            <div class="escopo-secundary">
+            <div class="escopo-secundary p-3">
                 {playlist.musicas.map((element) => {
                     return (
-                        <audio controls> <source src={element.video}></source></audio>
+                        <div className="playlist-line d-flex w-100 m-3">
+                            <img src={element.bgimg} style={{width:"100px", height:"100px", objectFit:"cover", marginRight: 10, borderRadius: 10}} />
+                            <div className="d-flex align-items-start justify-content-center flex-column m-2" style={{width: "80%"}}>
+                                <label>{element.video}</label>
+                                <audio controls className="w-100"> <source src={element.video} type="audio/ogg"></source></audio>
+                            </div>
+                        </div>
                     )
                 })}
 
