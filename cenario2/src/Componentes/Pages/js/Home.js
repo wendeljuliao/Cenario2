266ba56d005ac18f2d079fd4415211baf1ae8331
Wendel <wendelljuliao@hotmail.com>
2021-10-19 19:12:11 -0300

    db.json criado

--- cenario2/src/Componentes/Pages/js/Home.js
+++ cenario2/src/Componentes/Pages/js/Home.js
@@ -1,31 +1,44 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
+import axios from 'axios'
 import Card from '../../js/Card'
 
 import playlist from "../../../Mocks/playlist";
 import { Link } from "react-router-dom";
 
-export default class Home extends Component {
+export default function Home() {
+    const [playlists, setPlaylists] = useState([]);
 
-    render() {
-        return (
-            <div class="escopo-primary">
-                <div class="escopo-secundary">
-                    <div class="container">
+    useEffect(() => {
 
-                        <div class="row d-grid" style={{gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(2, 320px)', gridAutoFlow: 'row'}}>
+        axios.get("http://localhost:3001/playlists/")
+        .then(res => {
+            console.log(res.data)
+            setPlaylists(res.data)
+        })
 
-                            {playlist.map((item) => {
-                                return (
-                                    <Link style={{width: 390}} to={`/playlist/${item.id}`}><Card key={item.id} title={item.title} sub={item.sub} bgimg={item.imagem} num={item.id} /></Link>
-                                )
-                            }
-                            )}
-                        </div>
+    }, [])
+
+
+
+
+    return (
+        <div class="escopo-primary"> 
+            <div class="escopo-secundary">
+                <div class="container">
+                    <div class="row d-grid" style={{ gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(2, 320px)', gridAutoFlow: 'row' }}>
+
+                        {playlists.map((item) => {
+                            return (
+                                <Link style={{ width: 390 }} to={`/playlist/${item.id}`}><Card key={item.id} title={item.title} sub={item.sub} bgimg={item.imagem} num={item.id} /></Link>
+                            )
+                        }
+                        )}
                     </div>
-
                 </div>
+
             </div>
-        )
-    }
+        </div>
+    )
+
 }
