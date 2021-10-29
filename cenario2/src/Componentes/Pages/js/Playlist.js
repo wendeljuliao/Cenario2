9fcc1b890c799c44959395fcde271d0d19705ca7
GabrielCunha6403 <gabrielcunha2012@gmail.com>
2021-10-29 18:46:52 -0300

    botão para adicionar música na playlist

--- cenario2/src/Componentes/Pages/js/Playlist.js
+++ cenario2/src/Componentes/Pages/js/Playlist.js
@@ -31,13 +31,16 @@ export default function Playlist() {
             <div class="escopo-secundary p-3">
                 {musicas.map((element) => {
                     return (
-                        <div className="playlist-line d-flex w-100 m-3">
+                        <div className="playlist-line d-flex align-items-center justify-content-center w-100 m-3">
                             <img src={element.bgimg} style={{ width: "100px", height: "100px", objectFit: "cover", marginRight: 10, borderRadius: 10 }} />
-                            <div className="d-flex align-items-start justify-content-center flex-column m-2" style={{ width: "80%" }}>
-                                <label>{element.video}</label>
-                                <audio controls className="w-100"> <source src={element.video} type="audio/mp3"></source></audio>
-                                {console.log(element)}
-
+                            <div className="d-flex align-items-center justify-content-center flex-row m-2" style={{ width: "80%" }}>
+                                <div className="w-100">    
+                                    <label>{element.video}</label>
+                                    <audio controls className="w-100"> <source src={element.video} type="audio/mp3"></source></audio>
+                                    {console.log(element)}
+                                </div>
+                                <a><svg xmlns="http://www.w3.org/2000/svg" width="60px" height="60px" style={{fill: '#2e2e2e'}} viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
+                            </a>
                             </div>
                         </div>
                     )
