5ad9c6f8726b511abddc8624716e9f897ad0f15f
Wendel <wendelljuliao@hotmail.com>
2021-10-30 17:09:49 -0300

    adicionando e deletando playlists

--- cenario2/src/Componentes/js/Card.js
+++ cenario2/src/Componentes/js/Card.js
@@ -6,7 +6,7 @@ import { Redirect } from "react-router";
 
 export default class Card extends Component {
 
-    
+
 
     constructor(props) {
         super(props);
@@ -24,21 +24,33 @@ export default class Card extends Component {
 
     deletePlaylist(e, num, isDelete) {
         e.preventDefault()
+        const usuario = JSON.parse(localStorage.getItem('usuarioLogado'))
+
+
+        for (let i = 0; i < usuario.playlists.length; i++) {
+            if (usuario.playlists[i].id === num) {
+                usuario.playlists.splice(i, 1)
+                break
+            }
+        }
+
+
+        localStorage.setItem('usuarioLogado', JSON.stringify(usuario))
 
         console.log("...")
-        axios.delete(`http://localhost:3001/playlists/${num}`)
-            .then((res) => {console.log(res.data)})
+        axios.put(`http://localhost:3001/users/${usuario.id}`, usuario)
+            .then((res) => { console.log(res.data) })
         console.log("...")
 
         isDelete(true)
-        
+
     }
 
     render() {
-    
+
         return (
             <div class="line position-relative">
-                <a onClick={(e) => this.deletePlaylist(e, this.props.num, this.props.setIsDelete)} className="botaoFecha position-absolute m-3" style={{top: 0, right: 0}}><img src="../../../Images/close_icon.png" style={{width: '30px', height: 'auto'}}/></a>
+                <a onClick={(e) => this.deletePlaylist(e, this.props.num, this.props.setIsDelete)} className="botaoFecha position-absolute m-3" style={{ top: 0, right: 0 }}><img src="../../../Images/close_icon.png" style={{ width: '30px', height: 'auto' }} /></a>
                 <div class="body" style={{ backgroundImage: `url(${this.props.bgimg})` }}>
                     <label class="title-card">{this.props.title}</label>
                     <label class="sub-card">{this.props.sub}</label>
